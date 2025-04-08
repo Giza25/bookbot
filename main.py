44a73604e79bbdccd7de9249ed0fd0e1235c349f
Giza25 <dyma125@gmail.com>
2025-04-08 21:22:35 +0400

    Created stats.py in which there are statistics functions

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+from stats import get_num_of_words
+
 def get_book_text(path):
     with open(path) as f:   # Opening a file and storing it in f
         return f.read()     # Returning the entire file as a string
-
-def get_num_of_words(words):
-    list = words.split()
-    return len(list)
 
 def main():
     frankenstein = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,4 @@
+
+def get_num_of_words(words):
+    list = words.split()
+    return len(list)
