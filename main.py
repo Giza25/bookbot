08dc714410ca818faa2df499c3a6f897b39375ad
Giza25 <dyma125@gmail.com>
2025-04-08 22:08:36 +0400

    Sorted a dictionary of words. Added a nice text visual

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import get_num_of_words
+from stats import get_num_of_char
+from stats import sort_dict_by_value
 
 def get_book_text(path):
     with open(path) as f:   # Opening a file and storing it in f
@@ -7,6 +9,16 @@ def get_book_text(path):
 def main():
     frankenstein = get_book_text("books/frankenstein.txt")
     number_of_words = get_num_of_words(frankenstein)
-    print(f"{number_of_words} words found in the document")
+    number_of_characters = sort_dict_by_value(get_num_of_char(frankenstein))
+    print(f'''
+============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found {number_of_words} total words
+--------- Character Count -------''')
+    for char in number_of_characters:
+        if char["char"].isalpha():
+            print(f"{char["char"]}: {char["num"]}")
+    print("============= END ===============")
 
 main()
