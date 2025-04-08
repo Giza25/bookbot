29b4d8d97c6f957dad3ab19244f2650d550833b2
Giza25 <dyma125@gmail.com>
2025-04-08 22:12:16 +0400

    refactored printing into a separate function

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from stats import get_num_of_words
 from stats import get_num_of_char
 from stats import sort_dict_by_value
+import sys
 
 def get_book_text(path):
     with open(path) as f:   # Opening a file and storing it in f
         return f.read()     # Returning the entire file as a string
-
-def main():
-    frankenstein = get_book_text("books/frankenstein.txt")
-    number_of_words = get_num_of_words(frankenstein)
-    number_of_characters = sort_dict_by_value(get_num_of_char(frankenstein))
+    
+def print_bookbot(number_of_words, number_of_characters):
     print(f'''
 ============ BOOKBOT ============
 Analyzing book found at books/frankenstein.txt...
@@ -20,5 +18,12 @@ Found {number_of_words} total words
         if char["char"].isalpha():
             print(f"{char["char"]}: {char["num"]}")
     print("============= END ===============")
+
+def main():
+    frankenstein = get_book_text("books/frankenstein.txt")
+    number_of_words = get_num_of_words(frankenstein)
+    number_of_characters = sort_dict_by_value(get_num_of_char(frankenstein))
+    print_bookbot(number_of_words, number_of_characters)
+    
 
 main()
